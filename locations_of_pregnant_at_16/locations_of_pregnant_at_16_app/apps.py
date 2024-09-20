ccb15b7c487c60f803c203469eb5efa9b4ed10fc
mamssolivkami <polina_malakhova@inbox.ru>
2024-09-21 01:23:20 +0700

    Создан проект и добавлено приложение django

---
+++ locations_of_pregnant_at_16/locations_of_pregnant_at_16_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LocationsOfPregnantAt16AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'locations_of_pregnant_at_16_app'
